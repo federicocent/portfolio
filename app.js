830cd0aac42f4cb3d241c4d533967a4e2673a892
federicocent <fivegta2617@gmail.com>
2020-11-05 01:13:33 -0300

    x

---
+++ app.js
@@ -0,0 +1,12 @@
+var wow = new WOW({
+boxClass: 'wow',
+animateClass: 'animated',
+offset: 300,
+mobile: true,
+live: true,
+callback: function(box) {
+},
+scrollContainer: null,
+resetAnimation: true,
+});
+wow.init();
